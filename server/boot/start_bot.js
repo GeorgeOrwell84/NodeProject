2a76520532d54e66c3754fde22992fb82ab1daca
Jimmmy <gianbar90@gmail.com>
2019-01-19 17:33:34 +0100

    Msg added

--- server/boot/start_bot.js
+++ server/boot/start_bot.js
@@ -24,14 +24,14 @@ bot.on('text', async (msg)=>{
         input: msg.text
     })
 
-    console.log(new Date(msg.date.now()))
-    console.log(new Date(msg.date.now()))
+    // console.log(new Date(msg.date.now()))
+    // console.log(new Date(msg.date.now()))
 
-    // const body=await axios.get("http://localhost:3000/api/BotMemories/rememberAll")
+     const body=await axios.get("http://localhost:3000/api/BotMemories/rememberAll")
 
-    // body.data.memories.forEach(element => {
-    //     msg.reply.text(element.content)    
-    // })
+    body.data.memories.forEach(element => {
+         msg.reply.text(element.content)    
+     })
 
 })
 
